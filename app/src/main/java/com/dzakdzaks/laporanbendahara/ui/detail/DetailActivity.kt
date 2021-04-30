ddf4fd1000565c774ebc38bf4a6eb69cc1461eac
Friday <dzakyrahmanto@gmail.com>
2021-05-01 02:00:58 +0700

    add state

--- app/src/main/java/com/dzakdzaks/laporanbendahara/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/dzakdzaks/laporanbendahara/ui/detail/DetailActivity.kt
@@ -6,7 +6,9 @@ import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.activity.viewModels
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.dzakdzaks.laporanbendahara.R
 import com.dzakdzaks.laporanbendahara.data.remote.model.Report
@@ -33,6 +35,10 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
         setupView()
     }
 
@@ -48,7 +54,7 @@ class DetailActivity : AppCompatActivity() {
 
         intent?.getStringExtra(ACTION)?.let {
             if (it == ADD) {
-
+                setupAdd()
             } else {
                 setupDetail()
             }
@@ -59,42 +65,21 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupAdd() {
+        binding.apply {
+            containerType.visible()
+
+            conditionalViewByIsChecked(toggle.checkedRadioButtonId)
+            toggle.setOnCheckedChangeListener { _, checkedId -> conditionalViewByIsChecked(checkedId) }
+        }
+    }
+
     private fun setupDetail() {
         binding.apply {
+            containerType.gone()
             intent.getParcelableExtra<Report>(REPORT)?.let {
 
-                supportActionBar?.apply {
-                    setDisplayHomeAsUpEnabled(true)
-                    setDisplayShowHomeEnabled(true)
-                    title = if (it.type == Report.INCOME) "Pemasukan" else "Pengeluaran"
-
-                }
-
-                inputDate.apply {
-                    hint = if (it.type == Report.INCOME) "Tanggal Pemasukan" else "Tanggal Pengeluaran"
-                    editText?.setOnClickListener {
-                        showMaterialDatePicker(true, selectedTimeInMillis) { dateLong ->
-                            selectedTimeInMillis = dateLong
-                            val selectedDateString = Date(dateLong).convertDateToReadable()
-                            viewModel.date.value = selectedDateString
-                        }
-                    }
-                }
-
-                inputTitle.apply {
-                    hint = if (it.type == Report.INCOME) "Sumber Pemasukan" else "Jenis Pengeluaran"
-                    val adapter = ArrayAdapter(
-                            this@DetailActivity,
-                            R.layout.item_auto_complete,
-                            if (it.type == Report.INCOME) viewModel.getSourceIncome() else viewModel.getTypeExpense()
-                    )
-                    (editText as? AutoCompleteTextView)?.setAdapter(adapter)
-                }
-
-                inputTotal.apply {
-                    hint = if (it.type == Report.INCOME) "Jumlah Pemasukan" else "Jumlah Pengeluaran"
-
-                }
+                conditionalViewByReport(report = it)
 
                 tvTitleCheckbox.text = if (it.type == Report.INCOME) "Penerima & Saksi" else "Yang Mengeluarkan"
 
@@ -106,7 +91,80 @@ class DetailActivity : AppCompatActivity() {
                     inputWitness.visible()
                 }
 
-                generateCheckBox(it)
+            }
+        }
+    }
+
+    private fun conditionalViewByIsChecked(@IdRes checkedId: Int ) {
+        binding.apply {
+            val isExpense = checkedId == R.id.rbExpense
+            if (isExpense) {
+                inputReceiver.visible()
+                inputWitness.visible()
+            } else {
+                inputReceiver.gone()
+                inputWitness.gone()
+            }
+
+            tvTitleCheckbox.text = if (isExpense) "Yang Mengeluarkan" else "Penerima & Saksi"
+
+            inputDate.apply {
+                hint = if (isExpense) "Tanggal Pengeluaran" else "Tanggal Pemasukan"
+                editText?.setOnClickListener {
+                    showMaterialDatePicker(true, selectedTimeInMillis) { dateLong ->
+                        selectedTimeInMillis = dateLong
+                        val selectedDateString = Date(dateLong).convertDateToReadable()
+                        viewModel.date.value = selectedDateString
+                    }
+                }
+            }
+
+            inputTitle.apply {
+                hint = if (isExpense) "Jenis Pengeluaran" else "Sumber Pemasukan"
+                val adapter = ArrayAdapter(
+                        this@DetailActivity,
+                        R.layout.item_auto_complete,
+                        if (isExpense)  viewModel.getTypeExpense() else viewModel.getSourceIncome()
+                )
+                (editText as? AutoCompleteTextView)?.setAdapter(adapter)
+            }
+
+            inputTotal.apply {
+                hint = if (isExpense) "Jumlah Pengeluaran" else  "Jumlah Pemasukan"
+
+            }
+        }
+    }
+
+    private fun conditionalViewByReport(report: Report) {
+        binding.apply {
+
+            supportActionBar?.title = if (report.type == Report.INCOME) "Pemasukan" else "Pengeluaran"
+
+            inputDate.apply {
+                hint = if (report.type == Report.INCOME) "Tanggal Pemasukan" else "Tanggal Pengeluaran"
+                editText?.setOnClickListener {
+                    showMaterialDatePicker(true, selectedTimeInMillis) { dateLong ->
+                        selectedTimeInMillis = dateLong
+                        val selectedDateString = Date(dateLong).convertDateToReadable()
+                        viewModel.date.value = selectedDateString
+                    }
+                }
+            }
+
+            inputTitle.apply {
+                hint = if (report.type == Report.INCOME) "Sumber Pemasukan" else "Jenis Pengeluaran"
+                val adapter = ArrayAdapter(
+                        this@DetailActivity,
+                        R.layout.item_auto_complete,
+                        if (report.type == Report.INCOME) viewModel.getSourceIncome() else viewModel.getTypeExpense()
+                )
+                (editText as? AutoCompleteTextView)?.setAdapter(adapter)
+            }
+
+            inputTotal.apply {
+                hint = if (report.type == Report.INCOME) "Jumlah Pemasukan" else "Jumlah Pengeluaran"
+
             }
         }
     }
